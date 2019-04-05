58be930edaa8c4b4bd76079b488d4edbbf1f7d7b
Steven Duncan <steven.k.duncan@gmail.com>
2019-04-05 12:58:07 -0600

    getting max call stack on large inputs

--- takeHomeCodeChallenge.js
+++ takeHomeCodeChallenge.js
@@ -30,11 +30,50 @@ Keep in mind indices are not zero based
 // -Start of Code-                                                 ===
 
 const answer = (x, y) => {
+  //we'll start at 1, 1 in our pyramid. it has the box id of 1.
+  let currentCell = 1;
+  let currentRow = 1;
+  let currentColumn = 1;
 
+  const traverseRows = (curCell, curRow, maxRow) => {
+    if (curRow < maxRow) {
+      curRow++;
+      curCell = curCell + (curRow - 1);
+      let result = traverseRows(curCell, curRow, maxRow);
+      return result;
+    } else {
+      return curCell;
+    }
+  }
+
+  //this function will give us the value of whatever is in column 1 on y's row.
+  let colOneAtRow = traverseRows(currentCell, currentRow, y);
+
+  //to get the next box id in column 2 we will take y + 1 and add it to the box id we just found
+  const colIncreaseInit = y + 1;
+
+  const traverseColumns = (curCell, curCol, maxCol, incAmount) => {
+    if (curCol < maxCol) {
+      const nextColCell = curCell + incAmount;
+      incAmount++;
+      curCol++;
+      return traverseColumns(nextColCell, curCol, maxCol, incAmount)
+    } else {
+      return curCell;
+    }
+  }
+
+  let output = traverseColumns(colOneAtRow, currentColumn, x, colIncreaseInit);
+  return '' + output;
 }
 
 // -End of Code-                                                   ===
 
 // given tests:
 
-console.log(answer(3, 2));
+console.log(answer(3, 3), ' is equal to expected 13');
+console.log(answer(2, 2), ' is equal to expected 5');
+console.log(answer(2, 4), ' is equal to expected 12');
+console.log(answer(4, 1), ' is equal to expected 10');
+console.log(answer(4, 2), ' is equal to expected 14');
+console.log(answer(100000, 100000),' is equal to expected 20000000001');
