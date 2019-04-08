35109def6f8f1926267f3fb6a2152c4d98574d0e
Steven Duncan <steven.k.duncan@gmail.com>
2019-04-07 21:43:09 -0600

    updated messages content and placement

--- Javascript/React_Nextjs_UI/pages/components/Message.js
+++ Javascript/React_Nextjs_UI/pages/components/Message.js
@@ -3,27 +3,46 @@ const Message = (props) => {
   if (props.x && props.y && props.targetCellValue) {
     return (
       <div>
-        <div className='x'>X: {props.x}</div>
-        <div className='y'>Y: {props.y}</div>
-        <div className='target-cell'>Target: {props.targetCellValue}</div>
-        <div>{props.currentX != props.x ? `Current X Index: ${props.currentX}` : `target cell found`}</div>
-        <div>{props.currentY != props.y && props.currentX === 1 ? `Current Y Index: ${props.currentY}` : `target y axis found`}</div>
-        <div>Current Cell Value: {props.currentCellValue}</div>
-        <div>Increase Amount: {props.currentIncrease}</div>
-        <div>{
-          props.initialMessage ? `We\'ll start at cell ${props.currentX}, ${props.currentY} and iterate up by next row\'s index - 1`
-          : props.y != props.currentY ? `Next we'll iterate up the column until we find our target row`
-          : !props.rowFound ? `Now that we've found our row we can continue into the columns by making row index + 1 our increase amount.`
-          : props.x !== props.currentX ? `We iterate through the row by adding previous increase amount + 1 to our current cell`
-          : `current cell ${props.currentCellValue} = target cell ${props.targetCellValue}`}
+        <div className='target-wrapper wrapper-group'>
+          <div className='target-cell'>Target Box Id: {props.targetCellValue}</div>
+          <div className='x'>Target X Index: {props.x}</div>
+          <div className='y'>Target Y Index: {props.y}</div>
+        </div>
+        <div className='current-wrapper wrapper-group'>
+          <div>Current Box Id: {props.currentCellValue}</div>
+          <div>Current X Index: {props.currentX}</div>
+          <div>Current Y Index: {props.currentY}</div>
         </div>
         <div>{
+          props.initialMessage ? `We\'ll start at box index ${props.currentX}, ${props.currentY} and iterate up by next row\'s index - 1`
+          : props.y != props.currentY ? `Next we'll iterate up the column until we find our target row`
+          : !props.rowFound ? `Now that we've found our target row we can continue into the columns by making our increase ammount = row index + 1.`
+          : props.x !== props.currentX ? `We iterate through the row by adding previous increase amount + 1 to our current box id`
+          : `Target Found! Current Box Id ${props.currentCellValue} = Target Box Id ${props.targetCellValue}`}
+        </div>
+        <br/>
+        <div>{
+          props.y > props.currentY ? `Increase Amount: ${props.currentIncrease}`
+          : props.rowFound === false ? null
+          : props.currentX !== props.x ? `Increase Amount:  ${props.currentIncrease}`
+          : null
+          }</div>
+        <div>{
           props.y > props.currentY ?
-           `${props.currentCellValue} + ${props.currentIncrease} = Next cell value: ${props.currentCellValue + props.currentIncrease}`
+           `${props.currentCellValue} + ${props.currentIncrease} = Next Box Id: ${props.currentCellValue + props.currentIncrease}`
            : props.rowFound === false ? null
            : props.currentX !== props.x ? `${props.currentCellValue} + ${props.currentIncrease} = Next cell value: ${props.currentCellValue + props.currentIncrease}`
            : null
         }</div>
+        <style jsx>{`
+          .wrapper-group {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 10px;
+          }
+
+          
+        `}</style>
       </div>
     )
   } else {
